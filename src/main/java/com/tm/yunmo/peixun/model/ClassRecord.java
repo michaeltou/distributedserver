3a5d12ca8ecb9551d5ea17c422e1374d7de87ca1
michaeltou <mingdou@deepidata.com>
2017-06-18 22:55:51 +0800

    add getter and setter.

--- src/main/java/com/tm/yunmo/peixun/model/ClassRecord.java
+++ src/main/java/com/tm/yunmo/peixun/model/ClassRecord.java
@@ -49,6 +49,155 @@ private String creator;
     //
     private Date updateDate;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getClassCode() {
+        return classCode;
+    }
+
+    public void setClassCode(String classCode) {
+        this.classCode = classCode;
+    }
+
+    public String getClassSeqCode() {
+        return classSeqCode;
+    }
+
+    public void setClassSeqCode(String classSeqCode) {
+        this.classSeqCode = classSeqCode;
+    }
+
+    public String getPicturesList() {
+        return picturesList;
+    }
+
+    public void setPicturesList(String picturesList) {
+        this.picturesList = picturesList;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getAssistTeacher() {
+        return assistTeacher;
+    }
+
+    public void setAssistTeacher(String assistTeacher) {
+        this.assistTeacher = assistTeacher;
+    }
+
+    public int getAttendCnt() {
+        return attendCnt;
+    }
+
+    public void setAttendCnt(int attendCnt) {
+        this.attendCnt = attendCnt;
+    }
+
+    public int getQingJiaCnt() {
+        return qingJiaCnt;
+    }
+
+    public void setQingJiaCnt(int qingJiaCnt) {
+        this.qingJiaCnt = qingJiaCnt;
+    }
+
+    public int getKuangKeCnt() {
+        return kuangKeCnt;
+    }
+
+    public void setKuangKeCnt(int kuangKeCnt) {
+        this.kuangKeCnt = kuangKeCnt;
+    }
+
+    public int getBuKeCnt() {
+        return buKeCnt;
+    }
+
+    public void setBuKeCnt(int buKeCnt) {
+        this.buKeCnt = buKeCnt;
+    }
+
+    public String getStudentKeShi() {
+        return studentKeShi;
+    }
+
+    public void setStudentKeShi(String studentKeShi) {
+        this.studentKeShi = studentKeShi;
+    }
+
+    public String getTeacherKeShi() {
+        return teacherKeShi;
+    }
+
+    public void setTeacherKeShi(String teacherKeShi) {
+        this.teacherKeShi = teacherKeShi;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getShenPiId() {
+        return shenPiId;
+    }
+
+    public void setShenPiId(String shenPiId) {
+        this.shenPiId = shenPiId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
 }
