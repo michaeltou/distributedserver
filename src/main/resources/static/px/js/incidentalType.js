6660b64464d57bbb30e4c372bcbabd767872f586
huangqijun <251506188@qq.com>
2017-06-20 17:45:43 +0800

    new files add

--- src/main/resources/static/px/js/incidentalType.js
+++ src/main/resources/static/px/js/incidentalType.js
@@ -1,7 +1,7 @@
 $(function(){
 	
-		$('#newCourseType').click(function(){
-			$('.addCourseType').dialog("option","title", "新建课程类别").dialog('open');
+		$('#newIncidentalType').click(function(){
+			$('.addIncidentalType').dialog("option","title", "新建杂费").dialog('open');
 		});
 		
 		
@@ -19,34 +19,39 @@ $(function(){
 
 		$("body").on('click',".editButton", function(){
  			EditData($(this).get(0));
- 			$('.addCourseType').dialog("option","title", "编辑社员").dialog('open');
+ 			$('.addIncidentalType').dialog("option","title", "编辑杂费").dialog('open');
 		});
 		
 	
-		$('.addCourseType').dialog({
+		$('.addIncidentalType').dialog({
 			autoOpen:false,
 			resizable: false,
 			modal: true,
-			width:500,
-			height:200,
-			title:'新建课程类别',
+			width:450,
+			height:250,
+			title:'新建杂费',
 			buttons:{
 			'保存':function(){
-				if($("#courseTypeCodeText").val() == ""){
-					alert("类别编号不能为空。");
-					$("#courseTypeCodeText").focus();
+				if($("#incidentalNameText").val() === ""){
+					alert("杂费名称不能为空。");
+					$("#incidentalNameText").focus();
 					return;
-				}else if($("#courseTypeNameText").val() == ""){
-					alert("类别名称不能为空。");
-					$("#courseTypeNameText").focus();
+				}else if($("#incidentalSalePriceText").val() === ""){
+					alert("杂费售价不能为空。");
+					$("#incidentalSalePriceText").focus();
 					return;
+				} else  if ($("#courseTypeText").children('option:selected').val() === ""){
+                    alert("请选择课程类别。");
+                    $("#courseTypeText").focus();
+                    return;
 				}
 				//$(this).submit();
 				$(this).dialog('close');
 				//-----------------------------
 				var tr = $("<tr></tr>").attr("bgcolor","#FFFFFF"); 
-				tr.append($("<td></td>").html($("#courseTypeCodeText").val())); 
-				tr.append($("<td style='text-align:center;'></td>").html($("#courseTypeNameText").val())); 
+				tr.append($("<td></td>").html($("#incidentalNameText").val()));
+                tr.append($("<td></td>").html($("#courseTypeText").children('option:selected').val()));
+				tr.append($("<td ></td>").html($("#incidentalSalePriceText").val()));
 				tr.append($("<td></td>").html("<input type='button' name='deleteButton' value='删除' class='deleteButton'> <input type='button' name='editButton' value='编辑' class='editButton' >")); 
 				tr.appendTo($("tbody")); 
 				
@@ -57,7 +62,7 @@ $(function(){
 			}
 			},
 			open: function (event, ui) {
-			 $(".ui-dialog-titlebar-close", $(this).parent()).hide();
+			 		//$(".ui-dialog-titlebar-close", $(this).parent()).hide();
 			 },
 		
 		});
@@ -77,8 +82,16 @@ $(function(){
 
 function EditData(editRow){
 	var tr = editRow.parentNode.parentNode;
-	byId("courseTypeCodeText").value = (tr.cells[0]).innerHTML;
-	byId("courseTypeNameText").value = (tr.cells[1]).innerHTML;
+	byId("teachingMaterialNameText").value = (tr.cells[0]).innerHTML;
+    var all_options = document.getElementById("courseTypeText").options;
+    for (i=0; i<all_options.length; i++){
+        if (all_options[i].value == (tr.cells[1]).innerHTML)  // 根据option标签的ID来进行断  测试的代码这里是两个等号
+        {
+            all_options[i].selected = true;
+        }
+    }
+	byId("teachingMaterialBuyPriceText").value = (tr.cells[2]).innerHTML;
+    byId("teachingMaterialSalePriceText").value = (tr.cells[3]).innerHTML;
 }
 
 function DeleteData(delRow){
